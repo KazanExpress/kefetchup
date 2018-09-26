deddb93042ff874b26ded88c9b18696c98c06e6e
Karen <dev.kaskar2008@gmail.com>
2018-09-26 16:34:28 +0300

    tests-and-build.

--- dist/types/generic-client.d.ts
+++ dist/types/generic-client.d.ts
@@ -6,13 +6,53 @@ export declare type APIClientHandlers = {
 declare type Optional<T> = {
     [key in keyof T]?: T[key];
 };
+/**
+ * Default fetch handler
+ *
+ * @export
+ * @param {string} url
+ * @param {RequestInit} [options]
+ * @returns {Promise<Response>}
+ */
 export declare function defaultFetch(url: string, options?: RequestInit): Promise<Response>;
+/**
+ * Generic API client with default request
+ *
+ * @export
+ * @class GenericAPIClient
+ */
 export declare class GenericAPIClient {
     readonly baseURL: string;
     readonly clientConfig?: RequestInit | undefined;
     readonly handlers: APIClientHandlers;
     constructor(baseURL?: string, clientConfig?: RequestInit | undefined, handlers?: Optional<APIClientHandlers>);
+    /**
+     * Request method for making requests (duh)
+     *
+     * @param {string} url Url to make request
+     * @param {RequestInit} [fetchConfig] Default fetch config
+     * @param {boolean} [overrideDefaultConfig] Should override client base fetch config or not
+     * @returns {(Promise<Response | any>)}
+     * @memberof GenericAPIClient
+     */
     request(url: string, fetchConfig?: RequestInit, overrideDefaultConfig?: boolean): Promise<Response | any>;
+    /**
+     * Fast alias method for request
+     *
+     * @private
+     * @param {string} method HTTP method (GET, PUT, POST, etc)
+     * @param {string} url Url to make request
+     * @param {RequestInit} [fetchConfig] Default fetch config
+     * @param {boolean} [overrideDefaultConfig] Should override client base fetch config or not
+     * @returns {(Promise<Response | any>)}
+     * @memberof GenericAPIClient
+     */
+    private alias;
+    get(url: string, fetchConfig?: RequestInit, overrideDefaultConfig?: boolean): Promise<Response | any>;
+    put(url: string, fetchConfig?: RequestInit, overrideDefaultConfig?: boolean): Promise<Response | any>;
+    post(url: string, fetchConfig?: RequestInit, overrideDefaultConfig?: boolean): Promise<Response | any>;
+    patch(url: string, fetchConfig?: RequestInit, overrideDefaultConfig?: boolean): Promise<Response | any>;
+    delete(url: string, fetchConfig?: RequestInit, overrideDefaultConfig?: boolean): Promise<Response | any>;
 }
 export declare class ResponseException extends Error {
     status: ResponseErrors;
@@ -20,6 +60,13 @@ export declare class ResponseException extends Error {
     constructor(message: string, status: ResponseErrors, data?: Response | undefined);
     toString(): string;
 }
+/**
+ * Retrieve string from response status
+ *
+ * @export
+ * @param {number} [status=-1] Response status (200, 404, 500, etc)
+ * @returns {string}
+ */
 export declare function handleStatus(status?: number): string;
 export declare enum ResponseErrors {
     BadRequest = 400,
@@ -35,6 +82,7 @@ export declare enum ResponseErrors {
     Gone = 410,
     LengthRequired = 411,
     InvalidMedia = 415,
+    'I\'m a teapot' = 418,
     Unprocessable = 422,
     TooManyRequests = 429,
     ServerError = 500,
