869111829e35d7f2cffa36b6e95250615dd257a6
Raiondesu <a.raiondesu@gmail.com>
2018-11-04 23:46:18 +0300

    Improve withQuery documentation

--- src/util.ts
+++ src/util.ts
@@ -1,6 +1,16 @@
 /**
  * Encode an object into the plain URL as url-query-string
  *
+ * ```js
+withQuery('/list', {
+  amount: 5,
+  filters: ['price', 'date']
+})```
+ *
+ * returns
+ * ```js
+'/list?amount=5&filters=price,date'```
+ *
  * @param {String} url a url to encode params into
  * @param {Object} queryParams query params in object form
  * @returns url with encoded params
